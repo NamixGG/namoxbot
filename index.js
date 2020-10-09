dccd62ef2a7b258c1e6be6e40857d954499b9748
namixgg <danieldelima5504@gmail.com>
2020-10-09 20:23:21 -0300

    make it better

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const options = require('./options')
 
 
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+const host = 'localhost';
+const port = 3000;
+
+# use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+  console.log("Server started.......");
 });
 
 
