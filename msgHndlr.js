ce0b5d8e3a5501c6eca435850d5f86dfb0ac0b96
namixgg <danieldelima5504@gmail.com>
2020-10-10 21:37:59 -0300

    make it better

--- msgHndlr.js
+++ msgHndlr.js
@@ -12,7 +12,6 @@ const { help, snk, info, donate, readme, listChannel } = require('./lib/help')
 const { stdout } = require('process')
 const nsfw_ = JSON.parse(fs.readFileSync('./lib/NSFW.json'))
 const welkom = JSON.parse(fs.readFileSync('./lib/welcome.json'))
-const gifify = require('gifify');
 
 moment.tz.setDefault('Asia/Jakarta').locale('id')
 
@@ -40,15 +39,19 @@ module.exports = msgHandler = async (client, message) => {
         const mess = {
             wait: 'Processando.....',
             error: {
-                St: '[❗] Erro',
-                Qm: '[❗] Terjadi kesalahan, mungkin themenya tidak tersedia!',
-                Yt3: '[❗] Terjadi kesalahan, tidak dapat meng konversi ke mp3!',
-                Yt4: '[❗] Terjadi kesalahan, mungkin error di sebabkan oleh sistem.',
-                Ig: '[❗] Midia esta em uma conta privada',
-                Ki: '[❗] Bot tidak bisa mengeluarkan admin group!',
-                Ad: '[❗] Ti ak dapat menambahkan target, mungkin karena di private',
-                Iv: '[❗] Link não é do instagram!'
+                St: '📢 Ocorreu um erro no processo, Tente novamente',
+                Ig: '📢 Midia esta em uma conta privada',
+                Iv: '📢 Link não é do instagram!'
             }
+        }
+        function makeid(length) {
+            var result           = '';
+            var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var charactersLength = characters.length;
+            for ( var i = 0; i < length; i++ ) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
         }
 
         const time = moment(t * 1000).format('DD/MM HH:mm:ss')
@@ -64,6 +67,7 @@ module.exports = msgHandler = async (client, message) => {
         const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
         const uaOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         const isUrl = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi)
+        
         if (!isGroupMsg && command.startsWith('!')) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(msgs(command)), 'from', color(pushname))
         if (isGroupMsg && command.startsWith('!')) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(msgs(command)), 'from', color(pushname), 'in', color(formattedTitle))
         if (!isGroupMsg && !command.startsWith('!')) console.log('\x1b[1;33m~\x1b[1;37m>', '[\x1b[1;31mMSG\x1b[1;37m]', time, color(body), 'from', color(pushname))
@@ -104,50 +108,31 @@ module.exports = msgHandler = async (client, message) => {
         case '!stickergif':
         case '!stikergif':
         case '!sgif':
+
             if (isMedia) {
                 if (mimetype === 'video/mp4' && message.duration < 10 || mimetype === 'image/gif' && message.duration < 10) {
                     const mediaData = await decryptMedia(message, uaOverride)
-                    client.sendText(from, '[Espere] Transformando sua midia em um Sticker +- 1Minuto, caso nao responda em 1 minuto essa midia nao pode ser transformada em stricker devido as regras do whatsapp')
-                    const nuberof = Math.floor((Math.random()*1000000) + 1)
-                    const filename = `./media/${nuberof}.${mimetype.split('/')[1]}`
+                    client.reply(from, '📣 Trabalhando Nisso.....',id);
+                    var randname = makeid(10);
+                    var gifname = (`${randname}.gif`)
+                    const filename = `./media/${randname}.${mimetype.split('/')[1]}`
                     await fs.writeFileSync(filename, mediaData)
+                    fs.copyFile('./media/output.gif', `./media/${randname}.gif`, (err) => {
+                        if (err) throw err;
+                        console.log(`${gifname} Criado!`);
+                    });
 
+                    await exec(`gify ${filename} ./media/${randname}.gif --fps=30 --scale=240:240`, async function (error, stdout, stderr) {
+                        const gif = await fs.readFileSync(`./media/${randname}.gif`, { encoding: "base64" })
+                        
+                        await client.sendImageAsSticker(from, `data:image/gif;base64,${gif.toString('base64')}`)
+                    });
 
-
-                    var gif = fs.createWriteStream(`./media/${nuberof}.gif`);
-
-                    var options = {
-                        resize: '240:240',
-                        from: 30,
-                        to: 35
-                    };
-
-                    gifify(filename, options).pipe(gif);
-                    const gif = await fs.readFileSync(`./media/${nuberof}.gif`, { encoding: "base64" })
-                    console.log('3')
-                    await client.sendImageAsSticker(from, `data:image/gif;base64,${gif.toString('base64')}`)
-                    console.log('4')
-                    gif.on('close', function end() {
-                    console.log('gifified ' + input + ' to ' + output);
-                        });
-
-                    
-                    }
-                    
-
-                } else (
-                    client.sendText(from, '[❗] Utilizar gif/videos de até 10s!')
-                )
-                
+                } else (client.sendText(from, '📢 Utilizar GIFs ou Videos de até 10s!'));
             }
             break
 
 
-        case '!menu':
-        case '!help':
-        case '!ajuda':
-            client.sendText(from, help)
-            break
 
         
         case '!ig':
@@ -167,9 +152,7 @@ module.exports = msgHandler = async (client, message) => {
                 }
             break
         
-        case '!snk':
-            client.reply(from, snk, id)
-            break
+       
         }
     } catch (err) {
         console.log(color('[ERROR]', 'red'), err)
