47a1882c2a2bb0ea35545922a396daf3c6716202
namixgg <danieldelima5504@gmail.com>
2020-10-12 13:23:19 -0300

    make it better

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ server.listen(process.env.PORT || 80, () => {
 
 
 
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'bnamix.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
+
 const start = async (client = new Client()) => {
 		global.sclient = client
         global.sendingAnimatedSticker = []
